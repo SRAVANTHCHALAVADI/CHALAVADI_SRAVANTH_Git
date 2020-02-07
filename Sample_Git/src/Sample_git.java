21d41b00c22ead8bd07879ac9616f06ef5591a27
Sravanth Chalavadi <Sravanth Chalavadi@DESKTOP-I4NO74C>
2020-02-07 07:53:15 +0530

    Git Task 1

---
+++ Sample_Git/src/Sample_git.java
@@ -0,0 +1,10 @@
+public class Sample_git {
+	public void sayHi() {
+		System.out.println("Hi...,I am Sravanth.This is to push a sample code into git");
+	}
+	public static void main(String [] args) {
+		Sample_git s=new Sample_git();
+		s.sayHi();
+		
+	}
+}
